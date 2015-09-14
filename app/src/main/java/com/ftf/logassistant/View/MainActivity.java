4274860d607e50670e519d7d854438dbcbdd8470
gzu524 <skyshenfu@gmail.com>
2015-09-14 16:43:59 +0800

    radiogroup完整实现 fragment切换实现

--- app/src/main/java/com/ftf/logassistant/View/MainActivity.java
+++ app/src/main/java/com/ftf/logassistant/View/MainActivity.java
@@ -1,28 +1,56 @@
 package com.ftf.logassistant.View;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.ftf.logassistant.R;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements RadioGroup.OnCheckedChangeListener {
     @ViewInject(R.id.radio_top)
     private RadioGroup radioGroup;
     @ViewInject(R.id.radio_button_talk)
     private RadioButton radioButtonTalk;
-    @ViewInject(R.id.radio_button_sms)
-    private RadioButton radioButtonSms;
-    @ViewInject(R.id.radio_button_synchronou)
-    private RadioButton radioButtonSynchronou;
+    private android.support.v4.app.FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_top);
+        setContentView(R.layout.activity_main);
+        //沉浸式布局需要APT19
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         ViewUtils.inject(this);
+        fragmentManager=getSupportFragmentManager();
+        radioButtonTalk.setChecked(true);
+        radioGroup.setOnCheckedChangeListener(this);
+        changeFragment(new TalkFragment(),false);
     }
 
 
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId){
+            case R.id.radio_button_talk:
+                changeFragment(new TalkFragment(),true);
+                break;
+            case R.id.radio_button_sms:
+                changeFragment(new SmsFragment(),true);
+                break;
+            case R.id.radio_button_synchronou:
+                changeFragment(new SynchronouFragment(),true);
+                break;
+        }
+    }
+    public void changeFragment(Fragment fragment,boolean isFirst){
+        android.support.v4.app.FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_fragment,fragment);
+        if (!isFirst){
+            fragmentTransaction.addToBackStack(null);
+        }
+        fragmentTransaction.commit();
+
+    }
 }
---
+++ app/src/main/java/com/ftf/logassistant/View/SynchronouFragment.java
@@ -0,0 +1,19 @@
+package com.ftf.logassistant.View;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ftf.logassistant.R;
+
+public class SynchronouFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_synchronou,container,false);
+        return view;
+    }
+}
